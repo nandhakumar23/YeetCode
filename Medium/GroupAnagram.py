5821ea5494627d26b44a1352ab4ac3e4f9e27581
Nandhakumar Saravana <nsaravan@usc.edu>
2021-05-28 09:00:19 -0700

    Group Anagrams

--- Medium/GroupAnagram.py
+++ Medium/GroupAnagram.py
@@ -1,53 +1,80 @@
-#Current sol only passes 104/111 cases. too slow!
+### [49\. Group Anagrams](https://leetcode.com/problems/group-anagrams/)
+
+Difficulty: **Medium**  
+
+Related Topics: [Hash Table](https://leetcode.com/tag/hash-table/), [String](https://leetcode.com/tag/string/)
+
+
+Given an array of strings `strs`, group **the anagrams** together. You can return the answer in **any order**.
+
+An **Anagram** is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+**Example 1:**
+
+```
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+```
+
+**Example 2:**
+
+```
+Input: strs = [""]
+Output: [[""]]
+```
+
+**Example 3:**
+
+```
+Input: strs = ["a"]
+Output: [["a"]]
+```
+
+**Constraints:**
+
+*   `1 <= strs.length <= 10<sup>4</sup>`
+*   `0 <= strs[i].length <= 100`
+*   `strs[i]` consists of lower-case English letters.
+
+
+#### Solution
+
+Language: **Python**
+
+```python
 class Solution(object):
-    def groupAnagrams(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: List[List[str]]
-        """
-        
-        GroupedAnagrams = []
-        visited = []
-        
-        
-        for i in range(len(strs)):
-            if strs[i] in visited:
-                continue
-            aGroup = []
-            aGroup.append(strs[i])
-            print(aGroup)
-            visited.append(strs[i])
-            CheckHashTableOG = {}
-            for letter in strs[i]:
-                CheckHashTableOG[letter]=0
-            for letter in strs[i]:
-                CheckHashTableOG[letter]+=1
-            visited_copy = copy.deepcopy(visited)
-            for j in range(len(strs)):
-                if len(strs[j])!=len(strs[i]):
-                    continue
-                flag = True
-                if strs[j] in visited_copy:
-                    visited_copy.remove(strs[j])
-                    continue
-                CheckHashTable = CheckHashTableOG.copy()
-                for letter in strs[j]:
-                    if(letter in CheckHashTable and CheckHashTable[letter]>=1):
-                        CheckHashTable[letter] -=1
-                    else:
-                        flag = False
-                        break
-                
-                for key in CheckHashTable:
-                    if CheckHashTable[key]!=0:
-                        flag = False
-                
-                if flag==False:
-                    continue
-                else:
-                    aGroup.append(strs[j])
-                    visited.append(strs[j])
-            GroupedAnagrams.append(aGroup)
-        
-        return GroupedAnagrams
-        
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        
+        GroupedAnagrams = []
+        visited = []
+        hashmap = {}
+        
+        for i in range(len(strs)):
+            curr_temp =list(strs[i])
+            curr_temp.sort()
+            curr = ""
+            curr = curr.join(curr_temp)
+            
+            if(curr in hashmap):
+                hashmap[curr].append(strs[i])
+            else:
+                hashmap[curr]= []
+                hashmap[curr].append(strs[i])
+        
+        for key in hashmap:
+            AGroup = hashmap[key]
+            GroupedAnagrams.append(AGroup)
+        
+        return GroupedAnagrams
+        
+                
+                
+                
+            
+                
+                
+```
