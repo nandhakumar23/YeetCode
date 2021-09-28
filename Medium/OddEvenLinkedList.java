2d52eee90b13c0a55393eae6588ad4fb71752102
Nandhakumar Saravana <nandha23121999@gmail.com>
2021-09-28 15:28:19 -0700

    more

---
+++ Medium/OddEvenLinkedList.java
@@ -0,0 +1,79 @@
+### [328\. Odd Even Linked List](https://leetcode.com/problems/odd-even-linked-list/)
+
+Difficulty: **Medium**  
+
+Related Topics: [Linked List](https://leetcode.com/tag/linked-list/)
+
+
+Given the `head` of a singly linked list, group all the nodes with odd indices together followed by the nodes with even indices, and return _the reordered list_.
+
+The **first** node is considered **odd**, and the **second** node is **even**, and so on.
+
+Note that the relative order inside both the even and odd groups should remain as it was in the input.
+
+You must solve the problem in `O(1)` extra space complexity and `O(n)` time complexity.
+
+**Example 1:**
+
+![](https://assets.leetcode.com/uploads/2021/03/10/oddeven-linked-list.jpg)
+
+```
+Input: head = [1,2,3,4,5]
+Output: [1,3,5,2,4]
+```
+
+**Example 2:**
+
+![](https://assets.leetcode.com/uploads/2021/03/10/oddeven2-linked-list.jpg)
+
+```
+Input: head = [2,1,3,5,6,4,7]
+Output: [2,3,6,7,1,5,4]
+```
+
+**Constraints:**
+
+*   `n == `number of nodes in the linked list
+*   `0 <= n <= 10<sup>4</sup>`
+*   `-10<sup>6</sup> <= Node.val <= 10<sup>6</sup>`
+
+
+#### Solution
+
+Language: **Java**
+
+```java
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+      
+        if(head==null)
+        {
+            return head;
+        }
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode evenhead = even;
+        
+        while(even!=null && even.next!=null)
+        {
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+            odd = odd.next;
+            even = even.next;
+        }
+        
+        odd.next = evenhead;
+        return head;  
+    }
+}
+```
